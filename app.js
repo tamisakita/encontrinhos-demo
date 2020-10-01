6b148be6059ba81d93ff8017c3cf6b7a94372a21
isisabreu <isisrcabreu@gmail.com>
2020-10-01 20:33:26 -0300

    done app.js v1

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
 
 const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const appRoutes = require('./routes/appRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.get('/', (request, response, next ) => response.sendFile(__dirname + '/views/home.html'));
+mongoose.connect('mongodb://localhost/encontrinhos-database', {useUnifiedTopology:true, useNewUrlParser:true})
+    .then(()=> console.log('funcionou'));
+    .catch( error =>{
+        console.log(error);
+        throw new Error ('databse not working');
+    });
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/', appRoutes);
+app.use('/', authRoutes);
 
 app.listen(process.env.PORT, () => console.log(`My first app listening on port ${process.env.PORT}!`));
 
