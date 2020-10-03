8895de2a06890155a33143c803c07ae83f7aeb51
isisabreu <isisrcabreu@gmail.com>
2020-10-03 14:50:27 -0300

    database errors fixed

--- models/User.js
+++ models/User.js
@@ -11,6 +11,6 @@ const userSchema = new Schema({
   timestamps: true,
 });
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
