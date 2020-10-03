aa6d7b188b0e5ce41ac9a81033b10fc172b79027
Tami Pipoca <tamisakita@hotmail.com>
2020-10-03 18:00:49 -0300

    authRoutes, signup viw, login view done

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+require('./configs/session.config')(app);
+
 mongoose.connect('mongodb://localhost/encontrinhos-database', {useUnifiedTopology:true, useNewUrlParser:true})
-  
+
   .then(() => console.log('funcionou'))
   .catch( error => {
-        console.log(error);
-        throw new Error ('databse not working');
-    });
+    console.log(error);
+    throw new Error ('databse not working');
+  });
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
@@ -25,8 +27,8 @@ app.set('views', __dirname + '/views');
 hbs.registerPartials(__dirname + '/views/partial');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/', appRoutes);
 app.use('/', authRoutes);
+app.use('/', appRoutes);
 
 app.listen(process.env.PORT, () => console.log(`My first app listening on port ${process.env.PORT}!`));
 
