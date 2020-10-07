5bce89195edfe0e94245b0a15dd81eb404b25130
Tami Pipoca <tamisakita@hotmail.com>
2020-10-06 22:02:08 -0300

    fixed

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const Event = require('../models/Event');
 const protectedRoute = require('../middlewares/protectedRoutes');
 
