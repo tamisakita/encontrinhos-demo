0521173da41f0a98f981bf03584d0d363e0482c5
Tami Pipoca <tamisakita@hotmail.com>
2020-10-15 22:39:35 -0300

    connected to mongodb

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const hbs = require('hbs');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require("path");
+const cors = require("cors");
 
-const appRoutes = require('./routes/appRoutes');
-const authRoutes = require('./routes/authRoutes');
-const apiRoutes = require('./routes/apiRoutes');
+const appRoutes = require("./routes/appRoutes");
+const authRoutes = require("./routes/authRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 
-require('./configs/session.config')(app);
+require("./configs/session.config")(app);
 
-mongoose.connect('mongodb://localhost/encontrinhos-database', {useUnifiedTopology:true, useNewUrlParser:true})
-
-  .then(() => console.log('Mongo connected'))
-  .catch(error => {
-    console.log(error);
-    throw new Error ('database not working');
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log(`Conectado ao Banco ${process.env.PORT}`))
+  .catch((err) => {
+    throw new Error(err);
   });
 
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
-hbs.registerPartials(path.join(__dirname + '/views/partials'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.use('/', authRoutes);
-app.use('/', appRoutes);
-app.use('/api', apiRoutes);
+app.use("/", authRoutes);
+app.use("/", appRoutes);
+app.use("/api", apiRoutes);
 
-app.listen(process.env.PORT, () => console.log(`My first app listening on port ${process.env.PORT}!`));
+app.listen(process.env.PORT, () =>
+  console.log(`My first app listening on port ${process.env.PORT}!`)
+);
